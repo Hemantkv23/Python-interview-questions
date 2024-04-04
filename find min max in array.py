ebcc49fa38d386c9356ef471719036893c610ece
Hemant <43467503+Hemantkv23@users.noreply.github.com>
2024-04-04 14:06:29 +0530

    appended 2nd method

--- find min max in array.py
+++ find min max in array.py
@@ -15,12 +15,40 @@ def find_min_max(l):
     else:
         lmin = l[0]
         lmax = l[1]
-    for i in range(2,size-1):
+    for i in range(2,size):
         if l[i]>lmax:
             lmax = l[i]
         elif l[i]<lmin :
             lmin = l[i]
     return lmax, lmin
 
+def find_min_max2(l):
+    size = len(l)
+    if size % 2 ==0 :
+        if l[0]<l[1]:
+            lmin=l[0]
+            lmax=l[1]
+        else:
+            lmin = l[1]
+            lmax = l[0]
+    else:
+        lmin = lmax = l[0]
+
+    i = 1
+
+    while(i<size-1):
+        if l[i]<l[i+1]:
+            lmin = min(lmin, l[i])
+            lmax = max(lmax, l[i+1])
+        else:
+            lmin = min(lmin, l[i+1])
+            lmax = max(lmax, l[i])
+
+        i = i+2 #compairing in pairs
+    return lmax, lmin
+            
+
+
 l = [9,1,3,4,5,2,0]
 logger.info(find_min_max(l))
+logger.info(find_min_max2(l))
